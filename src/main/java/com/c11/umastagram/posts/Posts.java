7d1758c65b69494644060c79e7c33c04c9a866e4
Alexangelo O. G. <99701769+Strawberry64@users.noreply.github.com>
2025-10-29 21:31:41 -0700

    Post table (#28)
    * Added Posts Table and Controller
    
    * Added tests
    
    * Added Comments
    
    * Added Comments

---
+++ src/main/java/com/c11/umastagram/posts/Posts.java
@@ -0,0 +1,94 @@
+/*
+Author: Alexangelo Orozco Gutierrez
+Date: October 29, 2025
+Summary: Contains the post table and getters/setters for all information
+*/
+
+package com.c11.umastagram.posts;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "posts")
+public class Posts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private String userId;
+
+    @Column(name = "text", nullable = false, columnDefinition = "TEXT")
+    private String text;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "date_posted", nullable = false)
+    private LocalDateTime datePosted;
+
+    @Column(name = "likes", nullable = false, columnDefinition = "INT DEFAULT 0")
+    private int likes;
+
+    public Posts() {}
+
+    public Posts(String userId, String text, String image, LocalDateTime datePosted) {
+        this.userId = userId;
+        this.text = text;
+        this.image = image;
+        this.datePosted = datePosted;
+        this.likes = 0;
+    }
+
+
+    // getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public LocalDateTime getDatePosted() {
+        return datePosted;
+    }
+
+    public void setDatePosted(LocalDateTime datePosted) {
+        this.datePosted = datePosted;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+}
