54d5e171fb443da76f560f1c70b06199801b666a
Wei-HaiMing <ezedito@gmail.com>
2025-10-29 21:06:49 -0700

    header comment on user repo test

--- src/test/java/com/c11/umastagram/repository/UserRepositoryTest.java
+++ src/test/java/com/c11/umastagram/repository/UserRepositoryTest.java
@@ -1,3 +1,10 @@
+/*
+ * Author: Armando Vega
+ * Created: 29 October 2025
+ * Date Last Modified: 29 October 2025
+ * Last Modified By: Armando Vega
+ * Summary: User Repository Unit Test
+ */
 package com.c11.umastagram.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
