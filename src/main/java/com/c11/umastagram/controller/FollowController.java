274db64946f6693551ceaf0f8bb585664b690460
gabe-tommi <gabetgal2112@gmail.com>
2025-12-02 13:19:56 -0800

    reattempt at heroku testing

--- src/main/java/com/c11/umastagram/controller/FollowController.java
+++ src/main/java/com/c11/umastagram/controller/FollowController.java
@@ -1,17 +1,11 @@
 package com.c11.umastagram.controller;
 
+import com.c11.umastagram.model.Follow;
+import com.c11.umastagram.model.FriendRequest;
+import com.c11.umastagram.service.FollowService;
 import com.c11.umastagram.service.FriendRequestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import com.c11.umastagram.model.Follow;
-import com.c11.umastagram.model.FollowId;
-import com.c11.umastagram.service.FollowService;
-import com.c11.umastagram.model.FriendRequest;
-import com.c11.umastagram.model.FriendRequestId;
-import com.c11.umastagram.service.FriendRequestService;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -19,11 +13,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/friends")
 public class FollowController {
-    @Autowired
+
     private final FriendRequestService friendRequestService;
-    @Autowired
     private final FollowService followService;
 
+    @Autowired
     public FollowController(FriendRequestService friendRequestService, FollowService followService) {
         this.friendRequestService = friendRequestService;
         this.followService = followService;
@@ -36,14 +30,13 @@ public class FollowController {
 
     @GetMapping("/sendFriendRequest/{userId}/{friendId}")
     public FriendRequest sendFriendRequest(@PathVariable Long userId, @PathVariable Long friendId) {
-        FriendRequestService friendReqServ = new FriendRequestService();
         LocalDateTime now = LocalDateTime.now();
         FriendRequest fr = new FriendRequest(userId, friendId, now);
-        return friendReqServ.saveFriendRequest(fr);
+        return friendRequestService.saveFriendRequest(fr);
     }
 
     @GetMapping("/acceptFriendRequest/{userId}/{friendId}")
-    public Follow acceptFriendRequest(Long userId, Long friendId) {
+    public Follow acceptFriendRequest(@PathVariable Long userId, @PathVariable Long friendId) {
         LocalDateTime now = LocalDateTime.now();
         Follow follow = new Follow(userId, friendId, now);
 
@@ -51,22 +44,3 @@ public class FollowController {
         return followService.saveFollow(follow);
     }
 }
-
-/**
- * flamingo
- *             __
- *            /(`o
- *      ,-,  //  \\
- *     (,,,) ||   V
- *    (,,,,)\//
- *    (,,,/w)-'
- *    \,,/w)
- *    `V/uu
- *      / |
- *      | |
- *      o o
- *      \ |
- * \,/  ,\|,.  \,/
- *
- * https://www.asciiart.eu/animals/birds-water
- */
