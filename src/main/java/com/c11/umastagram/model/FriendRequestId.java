95b8ec6c481d15b67e7ecea55ae19ee203ea9a89
gabe-tommi <gabetgal2112@gmail.com>
2025-12-06 21:35:26 -0800

    added getters and setters to friendrequestid

--- src/main/java/com/c11/umastagram/model/FriendRequestId.java
+++ src/main/java/com/c11/umastagram/model/FriendRequestId.java
@@ -3,9 +3,6 @@ package com.c11.umastagram.model;
 import java.io.Serializable;
 import java.util.Objects;
 
-// This class represents a composite key for the FriendRequest entity
-// consisting of userRequestId and userTargetId.
-
 public class FriendRequestId implements Serializable {
     private Long userRequestId;
     private Long userTargetId;
@@ -17,13 +14,30 @@ public class FriendRequestId implements Serializable {
         this.userTargetId = userTargetId;
     }
 
+    // ADD THESE GETTERS AND SETTERS
+    public Long getUserRequestId() {
+        return userRequestId;
+    }
+
+    public void setUserRequestId(Long userRequestId) {
+        this.userRequestId = userRequestId;
+    }
+
+    public Long getUserTargetId() {
+        return userTargetId;
+    }
+
+    public void setUserTargetId(Long userTargetId) {
+        this.userTargetId = userTargetId;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         FriendRequestId that = (FriendRequestId) o;
         return Objects.equals(userRequestId, that.userRequestId) &&
-                Objects.equals(userTargetId, that.userTargetId);
+            Objects.equals(userTargetId, that.userTargetId);
     }
 
     @Override
